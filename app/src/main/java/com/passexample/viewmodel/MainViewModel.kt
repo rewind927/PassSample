95b59b0623dcc76f124f7e05a9466ca61d07d952
rewind927 <rewind927@gmail.com>
2020-08-29 22:16:50 +0800

    Adjust main viewmodel logic

--- app/src/main/java/com/passexample/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/passexample/viewmodel/MainViewModel.kt
@@ -5,29 +5,33 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.passexample.data.Pass
 import com.passexample.data.PassType
+import java.util.*
 
 class MainViewModel : ViewModel() {
-
-    private var totalTime: Long = System.currentTimeMillis()
-
-    private val _expiredDate = MutableLiveData<Long>()
-    val expiredDate: LiveData<Long> = _expiredDate
-
+    private var expiredTime: Date = Date()
+    private val _expiredDate = MutableLiveData<Date>()
+    val expiredDate: LiveData<Date> = _expiredDate
 
     fun adjustExpirationDate(pass: Pass) {
         if (pass.activate) {
             if (pass.passType == PassType.DAY) {
-                totalTime += 24 * 60 * 60 * 1000 * pass.amount
+                Calendar.getInstance().apply {
+                    this.time = expiredTime
+                    this.add(Calendar.DATE, pass.amount)
+                    this[Calendar.SECOND] = 59
+                    this[Calendar.MINUTE] = 59
+                    this[Calendar.HOUR_OF_DAY] = 23
+                    expiredTime = this.time
+                }
             } else if (pass.passType == PassType.HOUR) {
-                totalTime += 60 * 60 * 1000 * pass.amount
-            }
-        } else {
-            if (pass.passType == PassType.DAY) {
-                totalTime -= 24 * 60 * 60 * 1000 * pass.amount
-            } else if (pass.passType == PassType.HOUR) {
-                totalTime -= 60 * 60 * 1000 * pass.amount
+                Calendar.getInstance().apply {
+                    this.time = expiredTime
+                    this.add(Calendar.HOUR, pass.amount)
+                    expiredTime = this.time
+                }
+
             }
         }
-        _expiredDate.value = totalTime
+        _expiredDate.value = expiredTime
     }
 }
